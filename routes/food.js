422e94c47aa24476ddc9e9801307a473897fee81
macdallas <roylexstephane@gmail.com>
2024-12-30 02:42:28 +0100

    add routes for food

--- routes/food.js
+++ routes/food.js
@@ -6,7 +6,7 @@ const authMiddleware = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/', authMiddleware, foodController.all);
-router.post('/get-user-foods', authMiddleware, foodController.getUserFoods);
+router.get('/get-user-foods', authMiddleware, foodController.getUserFoods);
 router.get('/:id', authMiddleware, foodController.get);
 router.post('/', authMiddleware, foodController.create);
 router.post('/add-user-food', authMiddleware, foodController.addUserFood);
